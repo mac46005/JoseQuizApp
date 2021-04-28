915c10d6694290ad3920479c63b58285f2e6b10d
Marco Preciado <mac46005@gmail.com>
2021-04-27 22:47:14 -0500

    Premade Data

--- JoseQuizApp/JoseQuizApp/Logic/QuizManager.cs
+++ JoseQuizApp/JoseQuizApp/Logic/QuizManager.cs
@@ -44,35 +44,199 @@ namespace JoseQuizApp.Logic
             );
         }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         public async Task LoadDataOnce()
         {
-            string[] codes =
-            {
-                "G80","G81", "G82", "G83","G84","G85","G86","G87","G88","G89",
-                "G00","G01","G02","G03","G12","G13","G28","G40","G41", "G42",
-                "G43","G53","G43","G70","G71","G72","G73","G74","G76","G90","G91",
-                "G98","G99","M00","M01","M02", "M03","M04", "M05","M06", "M08",
-                "M09","M30","M97","M98","M99"
+            string[] answers2 =
+                        {
+                "Subprogram call of program number that defines pocket geometry",
+                "X position of starting hole",
+                "Y position of starting hole",
+                "Final depth of pocket",
+                "Incremental Z-axis depth starting from R plane",
+                "R plane clearance position",
+                "X-axis shift over cut increment",
+                "Y-axis shift over cut increment",
+                "Finishing cut allowance",
+                "Cutter compensation",
+                "Cutter compensation geometry offset register number",
+                "Feed rate"
             };
-            string[] questions =
+            string[] questions2 =
             {
-                "Cancel Canned Cycle","Drill Canned Cycle", "Spot Drill / Counterbore Canned Cycle",
-                "Peck Drill Deep Hole Canned Cycle","Tapping Canned Cycle","Bore in~Bore out Canned Cycle",
-                "Bore in~Stop~Rapid out Canned Cycle","Bore in~Manual Retract Canned Cycle","Bore~Dwell~Manual Retract Canned Cycle",
-                "Bore~Dwell~Bore out Canned Cycle","Rapid Positioning Motion","Linear Interpolation Motion",
-                "Circular Interpolation Motion CW","Circular Interpolation Motion CCW",
-                "Circular Pocket Milling CW","Circular Pocket Milling CCW",
-                "Machine Zero Return Thru Reference Point","G40”* “Cutter Compensation Cancel",
-                "2D Cutter Compensation Left","2D Cutter Compensation Right","Tool Length Compensation +"
-                ,"Machine Zero XYZ Positioning, Non-Modal","Work Offset Positioning Coordinate #1","Bolt Hole Circle with a Canned Cycle",
-                "Bolt Hole Arc with a Canned Cycle","Bolt Holes Along an Angle with a Canned Cycle","High Speed Peck Drill Canned Cycle”," +
-                "Reverse Tapping Canned Cycle","Fine Boring Canned Cycle","Absolute Positioning Command","Incremental Positioning Command",
-                "Canned Cycle Initial Point Return","Canned Cycle \"R\" Plane Return","Program Stop","Optional Program Stop",
-                "Program End","Spindle On, Clockwise","Spindle On, Counterclockwise","Spindle Stop","Tool Change","Coolant On",
-                "Coolant Off","Program End and Reset","Local Sub-Program Call","Sub Program Call",
-                "M97 Local Sub-Program or M98 Sub-Program Return or Loop Program"
+                "Define the P letter for the General Purpose Pocket Pilling",
+                "Define the X letter for the General Purpose Pocket Pilling",
+                "Define the Y letter for the General Purpose Pocket Pilling",
+                "Define the Z letter for the General Purpose Pocket Pilling",
+                "Define the Q letter for the General Purpose Pocket Pilling",
+                "Define the R letter for the General Purpose Pocket Pilling",
+                "Define the I letter for the General Purpose Pocket Pilling",
+                "Define the J letter for the General Purpose Pocket Pilling",
+                "Define the K letter for the General Purpose Pocket Pilling",
+                "Define the G41 G42 letter for the General Purpose Pocket Pilling",
+                "Define the D letter for the General Purpose Pocket Pilling",
+                "Define the F letter for the General Purpose Pocket Pilling"
             };
-            foreach (var item in codes)
+
+
+            string mess =
+                "G80 ," +
+                "Cancel Canned Cycle ," +
+                "G81," +
+                " Drill Canned Cycle ," +
+                "G82," +
+                " Spot Drill  Counterbore Canned Cycle ," +
+                "G83," +
+                " Peck Drill Deep Hole Canned Cycle," +
+                "G84," +
+                "Tapping Canned Cycle ," +
+                "G85," +
+                " Bore in~Bore out Canned Cycle," +
+                "G86," +
+                " Bore in~Stop~Rapid out Canned Cycle ," +
+                "G87," +
+                " Bore in~Manual Retract Canned Cycle," +
+                "G88," +
+                " Bore~Dwell~Manual Retract Canned Cycle," +
+                "G89," +
+                " Bore~Dwell~Bore out Canned Cycle," +
+                "G00," +
+                " Rapid Positioning Motion ," +
+                "G01," +
+                " Linear Interpolation Motion ," +
+                "G02," +
+                " Circular Interpolation Motion CW," +
+                "G03," +
+                " Circular Interpolation Motion CCW," +
+                "G12," +
+                " Circular Pocket Milling CW," +
+                "G13," +
+                " Circular Pocket Milling CCW," +
+                "G28," +
+                " Machine Zero Return Thru Reference Point," +
+                "G40," +
+                " Cutter Compensation Cancel ," +
+                "G41," +
+                " 2D Cutter Compensation Left," +
+                "G42," +
+                " 2D Cutter Compensation Right," +
+                "G43," +
+                " Tool Length Compensation +," +
+                "G53," +
+                " Machine Zero XYZ Positioning Non - Modal," +
+                "G54," +
+                " Work Offset Positioning Coordinate #1," +
+                "G70," +
+                " Bolt Hole Circle with a Canned Cycle," +
+                "G71," +
+                " Bolt Hole Arc with a Canned Cycle," +
+                "G72," +
+                " Bolt Holes Along an Angle with a Canned Cycle," +
+                "G73," +
+                " High Speed Peck Drill Canned Cycle," +
+                "G74," +
+                " Reverse Tapping Canned Cycle," +
+                "G76," +
+                " Fine Boring Canned Cycle," +
+                "G90," +
+                " Absolute Positioning Command ," +
+                "G91," +
+                " Incremental Positioning Command ," +
+                "G98," +
+                " Canned Cycle Initial Point Return ," +
+                "G99," +
+                " Canned Cycle R Plane Return," +
+                "M00," +
+                " Program Stop," +
+                "M01," +
+                " Optional Program Stop," +
+                "M02," +
+                " Program End," +
+                "M03," +
+                " Spindle On Clockwise," +
+                "M04," +
+                " Spindle On Counterclockwise," +
+                "M05," +
+                " Spindle Stop," +
+                "M06," +
+                " Tool Change," +
+                "M08," +
+                " Coolant On," +
+                "M09," +
+                " Coolant Off," +
+                "M30," +
+                " Program End and Reset," +
+                "M97," +
+                " Local Sub - Program Call," +
+                "M98," +
+                " Sub Program Call," +
+                "M99," +
+                " M97 Local Sub-Program or M98 Sub - Program Return or Loop Program,";
+
+            var ac = 0;
+            var qc = 0;
+            for (int i = 0; i < ((answers2.Length + questions2.Length)); i++)
+            {
+                if (i % 2 == 0)
+                {
+                    mess += answers2[ac] + ",";
+                    ac++;
+                }
+                else
+                {
+                    mess += questions2[qc] + ",";
+                    qc++;
+                }
+            }
+
+            var newmess = mess.Remove(mess.Length - 1);
+
+
+
+
+            var array = newmess.Split(',');
+            var splitVal = array.Length / 2;
+            string[] answers = new string[splitVal];
+            string[] questions = new string[splitVal];
+            ac = 0;
+            qc = 0;
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    answers[ac] = array[i];
+                    ac++;
+                }
+                else
+                {
+                    questions[qc] = array[i];
+                    qc++;
+                }
+            }
+
+            foreach (var item in answers)
             {
                 var am = Resolver.Resolve<AnswerModel>();
                 am.Solution = item;
@@ -80,7 +244,7 @@ namespace JoseQuizApp.Logic
             }
             var answerList = await _answerRepository.GetItems();
 
-            for (int i = 0; i < answerList.Count-1; i++)
+            for (int i = 0; i < answerList.Count - 1; i++)
             {
                 var qm = Resolver.Resolve<QuestionModel>();
                 qm.Query = questions[i];
@@ -88,6 +252,22 @@ namespace JoseQuizApp.Logic
                 await _questionRepository.AddItem(qm);
             }
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
